4491dee72f4ca706a7ebf1ffa11fd493341126e7
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2018-09-08 08:41:05 -0400

    Grading
    So much white space! This is a personal preference, no points removed. However next time let's try adding some comments in all that space so we know what we're building in here. You started out good, and then the comments just stoppped!
    
    Also, in future assignments I'd like to see a little bit better titles for your commits.

--- hw1/hw1-araja1.R
+++ hw1/hw1-araja1.R
@@ -3,24 +3,18 @@
 # Homework # 1
 # Andrew ID: araja1
 
-
 # Loading libraries
-
 library(shiny)
 library(dplyr)
 library(plotly)
 library(tibble)
 
-
 # Fetching the dataset mtcars into cars and changing the rowname to a column called "Model"
 cars <- as_tibble(rownames_to_column(mtcars))
 colnames(cars)[colnames(cars)=="rowname"] <- "Model"
 
-
-
 # Define UI for application 
 ui <- navbarPage("Cars",
-                 
                  tabPanel("Plot",
                           sidebarPanel(
                             selectInput("model_select",
@@ -29,7 +23,6 @@ ui <- navbarPage("Cars",
                                         multiple = TRUE,
                                         selectize = TRUE
                                         ),
-                                      
                           sliderInput("mpg_select",
                                       "MPG:",
                                       min = min(cars$mpg, na.rm = T),
@@ -39,16 +32,10 @@ ui <- navbarPage("Cars",
                                     ),
                           mainPanel(plotlyOutput("plot"))
                           ),
-                
                 tabPanel("Data",
                          fluidPage(dataTableOutput("table")) 
                         )
               )
-
-
-
-
-
 
 # Define server logic 
 server <- function(input, output) 
@@ -56,17 +43,11 @@ server <- function(input, output)
               output$plot <- renderPlotly({
                      ggplotly(ggplot(data=cars,aes(x=wt,y=hp,colour=factor(gear),text=paste("<b>", Model, ":</b> ")))+
                      geom_point()+
-                     labs(title="Weight vs Horse Power",x="Weight on Tons",y="Horse Power",colour="Gears")
+                     labs(title="Weight vs Horse Power",x="Weight on Tons",y="Horse Power",colour="Gears") # Yes, the superior "colour" spelling!
                      ,tooltip="text")
                     })
               output$table <- renderDataTable(cars)
 }
 
-
-
-
-
-
 # Run the application 
 shinyApp(ui = ui, server = server)
-
